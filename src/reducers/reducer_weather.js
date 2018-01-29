2fa5f1672e564367dc676a55757f5abbd62a4f88
unknown <you@example.com>
2018-01-28 16:58:22 -0800

    added weather_list

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,9 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+        return [ action.payload.data, ...state ]; // this will result in [ city, city, city ] NOT [ city, [ city, city ] ]
+    }
     return state;
 }
